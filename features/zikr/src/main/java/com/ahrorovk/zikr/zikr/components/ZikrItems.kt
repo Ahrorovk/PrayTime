dad1858056cbfef817c34719fa44397518a36cd2
Sunnatulloh06 <sunnatullohsaidzoda@gmail.com>
2024-11-19 22:54:31 +0500

    Made as u said, worked with parameters

--- features/zikr/src/main/java/com/ahrorovk/zikr/zikr/components/ZikrItems.kt
+++ features/zikr/src/main/java/com/ahrorovk/zikr/zikr/components/ZikrItems.kt
@@ -1,3 +1,5 @@
+package com.ahrorovk.zikr.zikr.components
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,25 +16,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-
 @Composable
-fun ZikrItem() {
-    var isSelected by remember { mutableStateOf(false) }
-
+fun ZikrItem(text: String, isSelected: Boolean, onClick: () -> Unit) {
     Card(
         modifier = Modifier
             .size(150.dp)
-            .padding(top = 50.dp, start = 20.dp)
-            .clickable { isSelected = !isSelected },
+            .padding(16.dp)
+            .padding(top = 40.dp)
+            .clickable { onClick() },
         colors = CardDefaults.cardColors(
-            containerColor = if(isSelected) Color.Blue else Color.White
-        ))
-    {
+            containerColor = if (isSelected) Color.Blue else Color.White
+        )
+    ) {
         Text(
-            text = "Zikr 1",
-            modifier = Modifier.padding(16.dp),
+            text = text,
+            modifier = Modifier.padding(16.dp, top = 20.dp),
             style = MaterialTheme.typography.titleLarge,
-            color = if(isSelected) Color.White else Color.Black
+            color = if (isSelected) Color.White else Color.Black
         )
     }
 }
+
+@Composable
+fun SingleZikrItem() {
+    var isSelected by remember { mutableStateOf(false) }
+
+    ZikrItem(
+        text = "My Zikr",
+        isSelected = isSelected,
+        onClick = { isSelected = !isSelected }
+    )
+}
+
