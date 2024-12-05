3d49344be105c3ec13e961fa4bff470a53e34d28
Sunnatulloh06 <sunnatullohsaidzoda@gmail.com>
2024-12-05 20:07:47 +0500

    made a Zikr Screen

--- features/zikr/src/main/java/com/ahrorovk/zikr/zikr/components/ZikrItem.kt
+++ features/zikr/src/main/java/com/ahrorovk/zikr/zikr/components/ZikrItem.kt
@@ -5,34 +5,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun ZikrItem(text: String, isSelected: Boolean, onClick: (Boolean) -> Unit) {
-    Card(
+fun ZikrItem(text: String, isSelected: Boolean, onClick: () -> Unit) {
+    Card (
         modifier = Modifier
-            .size(150.dp)
-            .padding(16.dp)
-            .padding(top = 40.dp)
-            .clickable { onClick(!isSelected) },
+            .size(180.dp)
+            .padding(30.dp)
+            .clickable { onClick() },
         colors = CardDefaults.cardColors(
-            containerColor = if (isSelected) Color.Blue else Color.White
+            containerColor = if(isSelected) Color(0xFF007FFD) else Color(0xFFFFFFFF)
         )
-    ) {
+    ){
         Text(
             text = text,
-            modifier = Modifier.padding(16.dp, top = 20.dp),
-            style = MaterialTheme.typography.titleLarge,
-            color = if (isSelected) Color.White else Color.Black
+            fontSize = 20.sp,
+            color = if(isSelected) Color.White else Color.Black,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
